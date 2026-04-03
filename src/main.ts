99cfe64e477e5f36f5100f4c454d2de4852530e3
Bruno <bruno.exler@gmail.com>
2026-04-03 21:48:36 +0200

    add: all questions

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,4 @@ import { loadQuestions, getASetOfQuestionsForPlayer } from "./questions.js";
 loadQuestions().then(() => {
     const questions = getASetOfQuestionsForPlayer();
     console.log("Quiz ready!", questions);
-    // TODO: start UI here
 });
